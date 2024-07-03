453087760e7083bc3ab95bb5a78e884bee7bb2b0
Summer <liuxia@a-smartivf.com>
2024-07-03 15:17:31 +0800

    feat: 解决importmeta飘红

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,5 +1,5 @@
-const { VITE_API_PATH } = import.meta.env;
-
+// const { VITE_API_PATH } = import.meta.env;
+const { VITE_API_PATH } = (import.meta as any).env;
 export const baseUrlApi = (url: string) =>
   process.env.NODE_ENV === "development"
     ? `/api${url}`
